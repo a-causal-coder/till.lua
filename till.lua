37a5bd2df96779b259ac3d14dd58df629a849520
a-causal-coder <lewisyuriclark@gmail.com>
2026-01-19 20:18:24 +0000

    Update till.lua

--- till.lua
+++ till.lua
@@ -1,8 +1,8 @@
 local till = {}
---local loop
---local oldUpdate
+local loop
+local oldUpdate
 
---[[function till.wait_till(key, how_long, on_end) 
+function till.wait_till(key, how_long, on_end) 
     loop = { key = key,
         limit = how_long, 
         end_msg = on_end, 
@@ -19,23 +19,4 @@ local till = {}
             print(loop.end_msg) loop.running = false 
         end 
     end
-end --]]
-
-function till.wait_till(key, how_long, on_end)
-    if key == "space" then 
-        key = " "
-    elseif key == "escape" then
-        key = "^["
-    end
-    local input
-    local time = 0
-    while input ~= key or  time > how_long do
-        input = io.read()
-        time = time + 1
-    end
-    print(on_end)
 end
-
-till.wait_till("a", 1, "it worked")
-
-return till
